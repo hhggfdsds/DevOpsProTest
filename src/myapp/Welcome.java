fb8e449fc5892559e1b6424c9168ad4910a88784
Nisha Sharma <124479532+hhggfdsds@users.noreply.github.com>
2023-10-31 13:13:30 -0700

    Happy Holidays

--- src/myapp/Welcome.java
+++ src/myapp/Welcome.java
@@ -7,6 +7,7 @@ public class Welcome {
 		System.out.println("Welcome to Code Lab Technology");
 		System.out.println(50*60);
 		System.out.println("Hurray we did it");
+		System.out.println("Happy Holidays");
 		
 		
 	}
