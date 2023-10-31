968ad63d1f22624238e475818ef822ce3946d2be
Nisha Sharma <124479532+hhggfdsds@users.noreply.github.com>
2023-10-31 13:04:58 -0700

    I am uploading my whole project

---
+++ src/myapp/Welcome.java
@@ -0,0 +1,13 @@
+package myapp;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Code Lab Technology");
+		System.out.println(50*60);
+		
+		
+	}
+
+}
